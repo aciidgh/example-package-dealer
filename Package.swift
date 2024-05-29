b6fa3139513f7998d5bd85ef53cb9d0eb251f381
Ankit Aggarwal <ankit.spd@gmail.com>
2024-05-29 07:57:04 -0700

    Update swift-argument-parser version

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
         .package(url: "https://github.com/apple/example-package-deckofplayingcards.git",
                  from: "3.0.0"),
         .package(url: "https://github.com/apple/swift-argument-parser.git",
-                 from: "0.4.4"),
+                 from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
